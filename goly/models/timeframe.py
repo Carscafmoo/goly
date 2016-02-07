c5e0da19c6caa32c9180a0459c0ada0da6f5368a
Carson Moore <Carscafmoo@gmail.com>
2016-02-06 16:44:40 -0800

    Initial basic timeframe functionality

--- goly/models/timeframe.py
+++ goly/models/timeframe.py
@@ -31,7 +31,8 @@ class Timeframe(Base, db.Model):
     def validate_start(self, key, start):
         # Start must begin at midnight
         assert start.hour == start.minute == start.second == start.microsecond == 0, "Timeframe must begin at midnight!"
-        if (self.frequency_name == 'monthly'): assert start.day == 1, "Monthly timeframes must begin on the first of the month"
+        if (self.frequency_name == 'weekly'): assert start.weekday() == 6, "Weekly timeframes must begin on Sunday"
+        elif (self.frequency_name == 'monthly'): assert start.day == 1, "Monthly timeframes must begin on the first of the month"
         elif (self.frequency_name == 'quarterly'): assert start.day == start.month % 3 == 1, "Quarterly timeframes must begin on January, April, July, or October 1"
         elif (self.frequency_name == 'yearly'): assert start.day == start.month == 1, "Yearly timeframes must begin on Jan 1"
 
@@ -44,5 +45,57 @@ class Timeframe(Base, db.Model):
         if (freq == 'monthly'): return self.start + dateutil.relativedelta.relativedelta(months=+1)
         if (freq == 'quarterly'): return self.start + dateutil.relativedelta.relativedelta(months=+3)
         if (freq == 'yearly'): return self.start + dateutil.relativedelta.relativedelta(years=+1)
+
+    def to_dict(self):
+        return {
+            "start": str(self.start),
+            "end": str(self.end),
+            "frequency": self.frequency_name
+        }
+
+    @classmethod 
+    def get_most_recent_week_start(self, time):
+        time = time + dateutil.relativedelta.relativedelta(days=-((time.weekday() + 1) % 7))
+        
+        return datetime.datetime(time.year, time.month, time.day)
+            
+
+    @classmethod
+    def get_timeframe(self, frequency, time):
+        """Return the timeframe at a given time, given the timeframe frequency
+
+        Note that timeframes include their start time but not their end time, so if time is e.g.
+        Jan 1, 2016 00:00:00, the correct yearly time frame is the one that begins Jan 1, 2016 00:00:00
+        """
+        if (frequency == 'daily'):
+            return Timeframe(frequency, datetime.datetime(time.year, time.month, time.day))
+        if (frequency == 'weekly'):
+            return Timeframe(frequency, self.get_most_recent_week_start(time))
+        if (frequency == 'monthly'): 
+            return Timeframe(frequency, datetime.datetime(time.year, time.month, 1))
+        if (frequency == 'quarterly'): 
+            return Timeframe(frequency, datetime.datetime(time.year, time.month - (time.month - 1) % 3, 1))
+        if (frequency == 'yearly'): 
+            return Timeframe(frequency, datetime.datetime(time.year, 1, 1))
+
+    @classmethod
+    def get_current_timeframe(self, frequency):
+        return self.get_timeframe(frequency, datetime.datetime.now())
+
+    @classmethod
+    def get_timeframes(self, frequency, start, end):
+        """Return an array of all timeframes between start and end, including the timeframes that include star and end"""
+        assert end >= start, "End must be after start."
+        
+        tfs = []
+        tf = self.get_timeframe(frequency, start)
+        while tf.end <= end:
+            tfs.append(tf)
+            tf = Timeframe(frequency, tf._calculate_end())
+
+        tfs.append(tf)
+
+        return tfs
+
 
 
--- tests/test_timeframe.py
+++ tests/test_timeframe.py
@@ -3,6 +3,7 @@ import unittest
 from goly.models.timeframe import Timeframe
 from goly import db
 import datetime
+import dateutil.relativedelta
 
 class TestTimeframe(unittest.TestCase):
     def test_init(self):
@@ -19,6 +20,9 @@ class TestTimeframe(unittest.TestCase):
             tf = Timeframe("daily", datetime.datetime(2016, 1, 1, 1))
         tf = Timeframe("daily", datetime.datetime(2016, 1, 1))
         self.assertEqual(tf.end, datetime.datetime(2016, 1, 2))
+
+        with self.assertRaisesRegexp(AssertionError, "Weekly timeframes must begin on Sunday"):
+            tf = Timeframe("weekly", datetime.datetime(2016, 1, 1))
 
         ## start date for monthly should always be the start of a month
         with self.assertRaisesRegexp(AssertionError, "Monthly timeframes must begin on the first"):
@@ -39,7 +43,7 @@ class TestTimeframe(unittest.TestCase):
         self.assertEqual(tf.end, datetime.datetime(2017, 1, 1))
 
         ## Should create a weekly timeframe if everything's cool
-        now = datetime.datetime(2016, 1, 1)
+        now = datetime.datetime(2016, 2, 7)
         tf = Timeframe("weekly", now)
         self.assertEqual(tf.start, now)
         self.assertEqual(tf.end, now + datetime.timedelta(7))
@@ -63,8 +67,93 @@ class TestTimeframe(unittest.TestCase):
         tf.frequency_name = "yearly"
         self.assertEqual(tf._calculate_end(), datetime.datetime(2017, 2, 28))
 
+    def test_get_most_recent_week_start(self):
+        ## Should work for all days of the week
+        for x in range(0, 7):
+            time = datetime.datetime(2016, 2, 7) ## known Sunday
+            test_time = time + dateutil.relativedelta.relativedelta(days=x)
+            self.assertEqual(time, Timeframe.get_most_recent_week_start(test_time))
+        
+        ## Should work around edge cases -- year and month breaks
+        time = datetime.datetime(2016, 1, 1)
+        self.assertEqual(datetime.datetime(2015, 12, 27), Timeframe.get_most_recent_week_start(time))
 
+    def test_get_timeframe(self):
+        ## Each test tests that it gets the correct start / endpoints, even if the time is the start point
+        tf = Timeframe.get_timeframe("daily", datetime.datetime(2016, 1, 1))
+        self.assertEqual(tf.start, datetime.datetime(2016, 1, 1))
+        self.assertEqual(tf.end, datetime.datetime(2016, 1, 2))
+        new_tf = Timeframe.get_timeframe("daily", datetime.datetime(2016, 1, 1, 23, 59, 59, 99999))
+        self.assertEqual(new_tf.to_dict(), tf.to_dict())
 
+        tf = Timeframe.get_timeframe("weekly", datetime.datetime(2016, 2, 7))
+        self.assertEqual(tf.start, datetime.datetime(2016, 2, 7))
+        self.assertEqual(tf.end, datetime.datetime(2016, 2, 14))
+        new_tf = Timeframe.get_timeframe("weekly", datetime.datetime(2016, 2, 13, 23, 59, 59, 99999))
+        self.assertEqual(new_tf.to_dict(), tf.to_dict())
+
+        tf = Timeframe.get_timeframe("monthly", datetime.datetime(2016, 2, 1))
+        self.assertEqual(tf.start, datetime.datetime(2016, 2, 1))
+        self.assertEqual(tf.end, datetime.datetime(2016, 3, 1))
+        new_tf = Timeframe.get_timeframe("monthly", datetime.datetime(2016, 2, 29, 23, 59, 59, 99999))
+        self.assertEqual(new_tf.to_dict(), tf.to_dict())
+
+        tf = Timeframe.get_timeframe("quarterly", datetime.datetime(2016, 1, 1))
+        self.assertEqual(tf.start, datetime.datetime(2016, 1, 1))
+        self.assertEqual(tf.end, datetime.datetime(2016, 4, 1))
+        new_tf = Timeframe.get_timeframe("quarterly", datetime.datetime(2016, 3, 31, 23, 59, 59, 99999))
+        self.assertEqual(new_tf.to_dict(), tf.to_dict())
+
+        tf = Timeframe.get_timeframe("yearly", datetime.datetime(2016, 1, 1))
+        self.assertEqual(tf.start, datetime.datetime(2016, 1, 1))
+        self.assertEqual(tf.end, datetime.datetime(2017, 1, 1))
+        new_tf = Timeframe.get_timeframe("yearly", datetime.datetime(2016, 12, 31, 23, 59, 59, 99999))
+        self.assertEqual(new_tf.to_dict(), tf.to_dict())
+
+    def test_get_timeframes(self):
+        with self.assertRaisesRegexp(AssertionError, "End must be after start"):
+            Timeframe.get_timeframes("yearly", datetime.datetime(2016, 1, 2), datetime.datetime(2016, 1, 1))
+
+        start = datetime.datetime(2016, 1, 1)
+        end = datetime.datetime(2016, 12, 31, 23, 59, 59, 99999)
+
+        tfs = Timeframe.get_timeframes("daily", start, end)
+        self.assertEqual(len(tfs), 366)
+        for x in tfs:
+            self.assertEqual(x.frequency_name, "daily")
+
+        tfs = Timeframe.get_timeframes("weekly", start, end)
+        self.assertEqual(len(tfs), 53)
+        for x in tfs:
+            self.assertEqual(x.start.weekday(), 6)
+            self.assertEqual(x.frequency_name, "weekly")
+
+        tfs = Timeframe.get_timeframes("monthly", start, end)
+        self.assertEqual(len(tfs), 12)
+        for x in tfs:
+            self.assertEqual(x.start.day, 1)
+            self.assertEqual(x.end.day, 1)
+            self.assertEqual(x.frequency_name, "monthly")
+
+        tfs = Timeframe.get_timeframes("quarterly", start, end)
+        self.assertEqual(len(tfs), 4)
+        for x in tfs:
+            self.assertEqual(x.start.day, 1)
+            self.assertEqual(x.end.day, 1)
+            self.assertEqual(x.start.month % 3, 1)
+            self.assertEqual(x.end.month % 3, 1)
+            self.assertEqual(x.frequency_name, "quarterly")
+            
+        tfs = Timeframe.get_timeframes("yearly", start, end)
+        self.assertEqual(len(tfs), 1)
+        for x in tfs:
+            self.assertEqual(x.start.day, 1)
+            self.assertEqual(x.end.day, 1)
+            self.assertEqual(x.start.month, 1)
+            self.assertEqual(x.end.month, 1)
+            self.assertEqual(x.frequency_name, "yearly")
+
+    
 
 
 
